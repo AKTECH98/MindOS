b6032dc1380deb4d90db4c701ae96421d5cc440d
AKTECH98 <anshulkiyawat@gmail.com>
2026-01-22 16:20:57 -0500

    Remove scripts from version control

--- scripts/run_dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Run Streamlit app with development environment variables
-export APP_ENV=dev
-set -a
-source .env.dev
-set +a
-exec streamlit run app.py --server.port $STREAMLIT_SERVER_PORT
